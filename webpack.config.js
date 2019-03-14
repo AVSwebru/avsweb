e60905cad64ec2fcdbddc724cd7dba218c802715
remzov <remz@MacBook-Pro-Nikita.local>
2019-03-14 19:12:51 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,129 @@
+const webpack = require('webpack');
+const path = require('path');
+const env = process.env.WEBPACK_ENV;
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const PATHS = {
+  source: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  entry: {
+    main: PATHS.source + '/index.js'
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 9000,
+    compress: true,
+    open: true,
+    overlay: true,
+    contentBase: PATHS.build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'css/main.css'
+            }
+          },
+          {
+            loader: 'extract-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: env === 'production' ? true : false,
+              url: false
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|webm|mp4|ogg|ogv)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]?[hash]',
+              outputPath: 'img/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]?[hash]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'TweenLite': path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
+      'TweenMax': path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
+      'TimelineLite': path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
+      'TimelineMax': path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
+      'ScrollMagic': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+      'animation.gsap': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+      'debug.addIndicators': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
